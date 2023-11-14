a7112dd7e4bb76506f1b266b9eae0323f4fc3554
Ydahyun <68465628+Ydahyun@users.noreply.github.com>
2023-11-14 11:02:24 +0900

    Solve Ex4
    The replicate function is used to create the required number of spaces, which are then appended to the front of the original string using the ++ operator.

--- Set2b.hs
+++ Set2b.hs
@@ -96,8 +96,12 @@ myGcd a b
 -- * you can combine strings with the ++ operator.
 -- * you can compute the length of a string with the length function
 
+-- leftpad :: String -> Int -> String
+-- leftpad = todo
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad str len
+    | length str < len = replicate (len - length str) ' ' ++ str
+    | otherwise        = str
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
