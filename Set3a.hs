19618697793c9ea310fb7a17269a263b53f2d931
Ydahyun <68465628+Ydahyun@users.noreply.github.com>
2023-11-30 13:28:56 +0900

    Clear #12
    #12. foldr함수는 리스트에 있는 모든 함수를 구성한다. 리스트의 오른쪽 끝에서 시작하여 각 요소에 대해 주어진 함수를 적용한다.

--- Set3a.hs
+++ Set3a.hs
@@ -284,7 +284,9 @@ sumRights xs = sum $ map (either (const 0) id) xs
 --   multiCompose [(3*), (2^), (+1)] 0 ==> 6
 --   multiCompose [(+1), (2^), (3*)] 0 ==> 2
 
-multiCompose fs = todo
+-- multiCompose fs = todo
+multiCompose :: [a -> a] -> (a -> a)
+multiCompose = foldr (.) id
 
 ------------------------------------------------------------------------------
 -- Ex 13: let's consider another way to compose multiple functions. Given
